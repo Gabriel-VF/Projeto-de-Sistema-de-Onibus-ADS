af3149a7fb8ca0f69ba811d396805a4a7521e86f
Gabriel-VF <vergabriel07@gmail.com>
2025-10-31 16:27:12 +0000

    Visualização dos assentos

---
+++ assentos.java
@@ -0,0 +1,29 @@
+package atividades.sistema_de_onibus;
+
+public class Onibus {
+    public static void main(String[] args) {
+        System.out.println(
+            "┌--──────────────--┐\n" +
+            "│  o               |\n" +
+            "│[%s][%s]..[%s][%s]│\n" +
+            "|                  |\n" +
+            "│[%s][%s]..[%s][%s]│\n" +
+            "|                  |\n" +
+            "│[%s][%s]..[%s][%s]│\n" +
+            "|                  |\n" +
+            "│[%s][%s]..[%s][%s]│\n" +
+            "|                  |\n" +
+            "│[%s][%s]..[%s][%s]│\n" +
+            "|                  |\n" +
+            "│[%s][%s]..[%s][%s]│\n" +
+            "|                  |\n" +
+            "│[%s][%s]..[%s][%s]│\n" +
+            "|                  |\n" +
+            "│[%s][%s]..[%s][%s]│\n" +
+            "|                  |\n" +
+            "│[%s][%s]..[%s][%s]│\n" +
+            "|                  |\n" +
+            "│[%s][%s]..[  WC  ]│\n" +
+            "└──────────────────┘");
+    }
+}
