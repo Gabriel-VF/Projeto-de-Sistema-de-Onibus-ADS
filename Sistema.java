b3232579fed03ec64628859b3b30080ce7aa2c05
Gabriel-VF <vergabriel07@gmail.com>
2025-11-14 08:42:18 -0300

    modified:   Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -8,7 +8,6 @@ public class Sistema {
 	final static char CHAR_CANTO_SUPERIOR_ESQUERDO = '┌';
 	final static char CHAR_CANTO_SUPERIOR_DIREITO = '┐';
 	static final int NUMERO_ASSENTOS = 46;
-	static final boolean[] LISTA_ASSENTOS_VAZIOS = new boolean[NUMERO_ASSENTOS];
 	static String OnibusASCII = (
 			"┌--──────────────--┐\n" +
 			"│  o               |\n" +
@@ -111,7 +110,7 @@ public class Sistema {
 			final int LARGURA) {
 		final int NUMERO_VALORES = 4;
 		String[] offset = new String[NUMERO_VALORES];
-		String[] linhasV = new String[NUMERO_VALORES];
+		String[] linhasVerticais = new String[NUMERO_VALORES];
 		int valorLength = String.format("%s", valor).length();
 
 		// -1 por causa do CHAR_VERTICAL final que vai ser adicionado
@@ -120,9 +119,9 @@ public class Sistema {
 		offset[2] = repetirChar(' ', LARGURA - OFFSET_ONIBUS_INFO - horario.length() - 1);
 		offset[3] = repetirChar(' ', LARGURA - OFFSET_ONIBUS_INFO - valorLength - 3); // Por causa do "R$" e CHAR_VERTICAL
 		for (int i = 0; i < NUMERO_VALORES; i++) {
-			linhasV[i] = offset[i] + CHAR_VERTICAL;
+			linhasVerticais[i] = offset[i] + CHAR_VERTICAL;
 		}
-		return linhasV;
+		return linhasVerticais;
 	}
 
 	// Imprime as opções de ônibus e mensagem de bem-vindo
@@ -133,13 +132,14 @@ public class Sistema {
 		// Reajusta largura da interface de acordo com necessidade. Por padrão é a mesma
 		// largura da mensagem de bem-vindo
 		int tamanhoLinhaHorizontal = definirLargura(listaRotas, listaNomes);
-		// -2 por causa dos cantos
+		// -2 para compensar pelos cantos que vão ser adicionados
 		if (tamanhoLinhaHorizontal < LARGURA_MENSAGEM_BEM_VINDO - 2) {
 			tamanhoLinhaHorizontal = LARGURA_MENSAGEM_BEM_VINDO - 2;
 		}
 		String linhaHorizontal = repetirChar(CHAR_HORIZONTAL, tamanhoLinhaHorizontal);
 		String[] linhasVerticais;
 
+		// Imprime opções
 		for (int i = 0; i < listaNomes.length; i++) {
 			linhasVerticais = criarLinhaVerticais(listaNomes[i], listaRotas[i], listaHorarios[i], listaValores[i],
 					tamanhoLinhaHorizontal + 2);
@@ -156,7 +156,7 @@ public class Sistema {
 	}
 
 	public static void main(String[] args) {
-		String[] listaNomes = { "onibus 1", "onibus 2", "onibus 3" };
+		String[] listaNomes = { "onibus 1", "onibus 2", "onibus 3", "onibus 4"};
 		boolean[][] mapasDeAssentos = new boolean[listaNomes.length][NUMERO_ASSENTOS]; // mapasDeAssentos[índice do ônibus][assento]
 		String[] listaDesenhosASCII = new String[listaNomes.length];
 		String[] listaRotas = new String[listaNomes.length];
@@ -176,25 +176,28 @@ public class Sistema {
 		listaRotas[0] = "Tauá -> Picos";
 		listaRotas[1] = "Tauá -> Fortaleza";
 		listaRotas[2] = "Tauá -> Juazeiro do Norte";
+		listaRotas[3] = "Tauá -> Quixadá";
 		listaHorarios[0] = "16:30 -> 22:00";
 		listaHorarios[1] = "17:00 -> 00:00";
 		listaHorarios[2] = "19:00 -> 22:00";
+		listaHorarios[3] = "20:00 -> 01:00";
 		listaValores[0] = 100.5;
 		listaValores[1] = 80.5;
 		listaValores[2] = 110.2;
+		listaValores[3] = 80.0;
 
 		while (true) {
 			imprimirOnibusInfo(listaNomes, listaRotas, listaHorarios, listaValores);
-			System.out.print(String.format("Escolha seu ônibus [1-%s]: ", listaNomes.length));
+			System.out.print(String.format("Escolha seu ônibus [1-%s] ou digite 0 para sair: ", listaNomes.length));
 			escolhaDeOnibus = scanner.nextInt() - 1;
 			if (escolhaDeOnibus < 0 || escolhaDeOnibus > listaNomes.length - 1) {
 				break;
 			}
 			System.out.println(listaDesenhosASCII[escolhaDeOnibus]);
-			System.out.print(String.format("Insira seu assento [1-%s]: ", NUMERO_ASSENTOS));
+			System.out.print(String.format("Escolha seu assento [1-%s] ou digite 0 para voltar: ", NUMERO_ASSENTOS));
 			escolhaDeAssento = scanner.nextInt() - 1;
 			if (escolhaDeAssento < 0 || escolhaDeAssento > NUMERO_ASSENTOS - 1) {
-				break;
+				continue;
 			}
 			if (!mapasDeAssentos[escolhaDeOnibus][escolhaDeAssento]) {
 				mapasDeAssentos[escolhaDeOnibus][escolhaDeAssento] = true;
